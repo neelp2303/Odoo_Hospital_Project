ac504b1272bdd46d616b50f3889f246b27e0d449
neelp2303 <neelp6475@gmail.com>
2025-02-20 09:43:48 +0530

    modified:   conf/odoo.conf 	modified:   custom_modules/hospital_management/__manifest__.py

--- custom_modules/hospital_management/__manifest__.py
+++ custom_modules/hospital_management/__manifest__.py
@@ -3,7 +3,7 @@
     "description": """A Hospital Management app from Heliconia Solutions""",
     "summary": "A simple Hospital Management app",
     "version": "0.0.0",
-    "category": "Healthcare",
+    "category": "Health and Fitness",
     "sequence": 0,
     "website": "https://www.heliconia.io",
     "author": "Neel Patel (Heliconia Solutions Pvt. Ltd.), ",
