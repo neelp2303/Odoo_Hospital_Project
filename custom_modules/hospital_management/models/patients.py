70db35f36c18c6157fd219fced2ad675d61a8f2e
neelp2303 <neelp6475@gmail.com>
2025-02-25 17:50:50 +0530

    modified:   custom_modules/hospital_management/models/patients.py

--- custom_modules/hospital_management/models/patients.py
+++ custom_modules/hospital_management/models/patients.py
@@ -7,14 +7,11 @@ class patients_data(models.Model):
 
     name = fields.Char("Name", required=True)
     age = fields.Integer("Age")
-    gender = fields.Selection(
-        [("male", "Male"), ("female", "Female"), ("other", "Other")], "Gender"
-    )
+    gender = fields.Selection([("male", "Male"), ("female", "Female")], "Gender")
     dob = fields.Date("Dob")
     contact_number = fields.Char("Contact Number")
     address = fields.Text("Address")
     admission_date = fields.Date(default=fields.Datetime.now)
     discharge_date = fields.Date("Discharge Date")
     doctor_id = fields.Many2one("hospital.doctor", string="Doctor", required=True)
-    pat_image = fields.Image("Patient Image") 
-
+    pat_image = fields.Image("Patient Image")
