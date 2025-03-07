169f0ae6b8bbd4c8d807054e2417f7d9c27d1b57
neelp2303 <neelp6475@gmail.com>
2025-03-07 16:04:04 +0530

    modified:   custom_modules/hospital_management/models/conference.py 	modified:   custom_modules/hospital_management/views/conference_data.xml

--- custom_modules/hospital_management/models/conference.py
+++ custom_modules/hospital_management/models/conference.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Conference(models.Model):
@@ -8,3 +8,21 @@ class Conference(models.Model):
     name = fields.Char("Conference Name", required=True)
     date = fields.Date("Conference Date")
     doctor_ids = fields.Many2many("hospital.doctor", string="Attending Doctors")
+    doctor_count = fields.Integer(
+        string="Doctor Count", compute="_compute_doctor_count"
+    )
+
+    @api.depends("doctor_ids")
+    def _compute_doctor_count(self):
+        for record in self:
+            record.doctor_count = len(record.doctor_ids)
+
+    def action_attending_doctors(self):
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Attending Doctors",
+            "res_model": "hospital.doctor",
+            "view_mode": "list,form",
+            "domain": [("id", "in", self.doctor_ids.ids)],
+            "target": "current",
+        }
