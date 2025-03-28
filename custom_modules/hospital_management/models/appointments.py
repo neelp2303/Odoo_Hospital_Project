52b44309fe2c04a93e247c33a0463ba7ad949308
neelp2303 <neelp6475@gmail.com>
2025-03-28 16:23:06 +0530

    Modified

--- custom_modules/hospital_management/models/appointments.py
+++ custom_modules/hospital_management/models/appointments.py
@@ -6,8 +6,11 @@ from odoo import models, fields, api
 class HospitalAppointment(models.Model):
     _name = "hospital.appointment"
     _description = "Hospital Appointment"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
     _rec_name = "patient_id"
     patient_id = fields.Many2one("hospital.patient", string="Patient", required=True)
+    ref = fields.Char("Ref", related="patient_id.ref")
+
     patient_image = fields.Image(
         related="patient_id.image", string="Patient Image", store=True
     )
