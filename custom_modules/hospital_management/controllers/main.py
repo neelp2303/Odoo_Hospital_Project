a678b5e51a9fac789cadb8dc7294a31571775d1c
neelp2303 <neelp6475@gmail.com>
2025-04-16 15:40:50 +0530

    Modified

--- custom_modules/hospital_management/controllers/main.py
+++ custom_modules/hospital_management/controllers/main.py
@@ -1,28 +1,12 @@
-# -*- coding: utf-8 -*-
-# controllers/main.py
-
-import base64
 from odoo import http
-from odoo.http import request, content_disposition
+from odoo.http import request
 
 
-class PatientExportController(http.Controller):
-    @http.route("/web/hospital/export_csv", type="http", auth="user")
-    def export_csv(self, wizard_id, **kw):
-        wizard = request.env["hospital.patient.export.wizard"].browse(int(wizard_id))
+class HospitalPatientController(http.Controller):
 
-        if not wizard or not wizard.export_data:
-            return request.not_found()
-
-        # Decode the data
-        csv_content = base64.b64decode(wizard.export_data)
-
-        # Return with proper headers to ensure it's recognized as CSV
-        return request.make_response(
-            csv_content,
-            headers=[
-                ("Content-Type", "text/csv; charset=utf-8"),
-                ("Content-Disposition", content_disposition(wizard.export_filename)),
-                ("Content-Length", len(csv_content)),
-            ],
+    @http.route("/hospital/patients", type="http", auth="public", website=True)
+    def list_patients(self, **kwargs):
+        patients = request.env["hospital.patient"].sudo().search([])
+        return request.render(
+            "hospital_management.patient_list_template", {"patients": patients}
         )
